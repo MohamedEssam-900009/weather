25ec63c9d5f280e235ea633544284ae6c7e66d67
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2022-10-11 03:10:13 +0200

    Data Layer DataSource

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,3 +1,4 @@
 class AppConstants{
   static const String baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static const String appKey = '59714a6b3f3c31373a0097cc8516eebe';
 }
---
+++ lib/weather/data/datasource/remote_datasource.dart
@@ -0,0 +1,24 @@
+
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:weather/core/utils/constants.dart';
+import 'package:weather/weather/data/models/weather_model.dart';
+
+abstract class BaseRemoteDataSource {
+  Future<WeatherModel?> getWeatheByCountryName(String countryName);
+}
+
+class RemoteDataSource implements BaseRemoteDataSource {
+  @override
+  Future<WeatherModel?> getWeatheByCountryName(String countryName) async{
+    try {
+    var response = await Dio().get('${AppConstants.baseUrl}/weather?q=$countryName&appid=${AppConstants.appKey}');
+    print(response);
+    return WeatherModel.fromJson(json.decode(response.data));
+  } catch (e) {
+    print(e);
+    return null;
+  }
+  }
+}
