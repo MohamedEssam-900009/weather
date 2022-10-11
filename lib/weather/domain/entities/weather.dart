552a6065a83d9fd66cb024516820f6a6d01215ec
Mohamed Essam <77198018+MohamedEssam-900009@users.noreply.github.com>
2022-10-11 02:07:25 +0200

    Data layer Models

--- lib/weather/domain/entities/weather.dart
+++ lib/weather/domain/entities/weather.dart
@@ -5,11 +5,11 @@ class Weather {
   final String description;
   final int pressure;
 
-  Weather({
-    required this.id,
-    required this.cityName,
-    required this.main,
-    required this.description,
-    required this.pressure,
-  });
+  Weather(
+     this.id,
+     this.cityName,
+     this.main,
+     this.description,
+     this.pressure,
+  );
 }
